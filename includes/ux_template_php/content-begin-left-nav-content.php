b7e504ea80b069969a20436d956d80405c09246b
ViciousVictor <muyotofu@gmail.com>
2013-08-08 09:32:23 -0400

    added area for breadcrumbs

---
+++ includes/ux_template_php/content-begin-left-nav-content.php
@@ -0,0 +1,7 @@
+					</nav>
+
+				</div>
+
+				<div class="grid_12">
+					<a name="content-anchor" id="content-anchor" tabindex="-1"></a>
+					
--- includes/ux_template_php/header4.php
+++ includes/ux_template_php/header4.php
@@ -44,5 +44,3 @@
 					</div>	
 				</noscript>
 				
-				<a name="content-anchor" id="content-anchor" tabindex="-1"></a>
-<div id="content" class="grid_16 margin-bottom-none">
